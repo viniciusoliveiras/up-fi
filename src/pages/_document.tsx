2a2ba1febc3c6518ff025d60bdfb356267ca28f7
viniciusoliveiras <vinitag190@gmail.com>
2021-06-07 14:47:34 -0300

    index file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="logo.svg" type="image/svg" />
+          <title>up fi</title>
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,37 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface fetchImagesResponse {
+  data: Card[];
+  after: string | null;
+}
+
 export default function Home(): JSX.Element {
+  async function fetchImages({
+    pageParam = null,
+  }): Promise<fetchImagesResponse> {
+    if (pageParam) {
+      const { data } = await api.get(`/api/images`, {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return data;
+    }
+
+    const { data } = await api.get(`/api/images`);
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -19,17 +49,33 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    fetchImages,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => lastPage.after ?? null,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    let formattedDataTotal = [] as Card[];
+    const dataPages = data?.pages;
+
+    dataPages?.map(page => {
+      formattedDataTotal = [...formattedDataTotal, ...page.data];
+      // eslint-disable-next-line no-useless-return
+      return;
+    });
+
+    return formattedDataTotal;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +83,7 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && <Button>Carregar mais</Button>}
       </Box>
     </>
   );
